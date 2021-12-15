0ac261c3125ae85811b55822e07acc4ffb342ed6
indeskyz <codishazey@yahoo.ca>
2021-12-14 22:18:25 -0500

    created navbar and footer

---
+++ scripts/navbar.js
@@ -0,0 +1,24 @@
+const searchBar = document.querySelector("#search-bar");
+const openSearch = document.querySelector("#search-icon");
+const bookmarkIcon = document.querySelector("#bookmark-icon");
+const closeSearch = document.querySelector("#search-close");
+const navigationLinks = document.querySelector(".navigation-links");
+
+openSearch.onclick = () => {
+  searchBar.style.display = "block";
+  closeSearch.style.opacity = 1;
+  searchBar.style.animation = "fade_in_show .4s ease-out";
+  closeSearch.style.animation = "fade_in_show";
+  searchBar.focus();
+  openSearch.style.opacity = 0;
+  bookmarkIcon.style.opacity = 0;
+  navigationLinks.style.opacity = 0;
+};
+
+closeSearch.onclick = () => {
+  searchBar.style.display = "none";
+  closeSearch.style.opacity = 0;
+  openSearch.style.opacity = 1;
+  bookmarkIcon.style.opacity = 1;
+  navigationLinks.style.opacity = 1;
+};
